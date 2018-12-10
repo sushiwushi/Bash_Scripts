ddc0921ea6478d1f01b26490fd80b441487fab81
sushiwushi <45194995+sushiwushi@users.noreply.github.com>
2018-12-10 17:04:28 +0800

    Update youtube_dl.sh

--- youtube_dl.sh
+++ youtube_dl.sh
@@ -1,13 +1,15 @@
 #author: sushiwushi
-#!/bin/bash
-set -e
+#!/bin/bash -e
 if [[ $# -eq 0 ]] ; then
     echo "Usage: ./youtube_dl <URL>"
     exit 0
 fi
 echo "URL=$1"
-echo "Checking for updates..."
-sudo pip install --upgrade youtube_dl
-echo "Downloading $1 now..."
-youtube-dl -x -f bestaudio -o "~/Music/%(title)s.%(ext)s" $1
-echo "✺◟( ͡° ͜ʖ ͡°)◞✺  Done"
+if youtube-dl -x -f bestaudio -o "~/Music/%(title)s.%(ext)s" $1; then
+  echo "✺◟( ͡° ͜ʖ ͡°)◞✺  Done"
+else
+  echo "Checking for updates..."
+  sudo pip install --upgrade youtube_dl
+  youtube-dl -x -f bestaudio -o "~/Music/%(title)s.%(ext)s" $1
+  echo "✺◟( ͡° ͜ʖ ͡°)◞✺  Done"
+fi
